bab6536c6e1b01214b3311dd60b845b3f04f8cea
Mohammad Nayef <nayeftechno@gmail.com>
2021-11-24 16:54:42 +0200

    Commit Changes

---
+++ hooks/useMapStoreStateToClientState.js
@@ -0,0 +1,18 @@
+import {autorun} from "mobx";
+import {useState,useEffect} from "react";
+
+function useMapStateToClientState(store,storePropsSelectionFunction){
+    
+    const [state,setState] = useState(storePropsSelectionFunction(store));
+
+    useEffect(()=>{
+        autorun(()=>{
+            setState(storePropsSelectionFunction(store));
+        });
+    },[]);
+
+    return [state];
+
+};
+export {useMapStateToClientState};
+
