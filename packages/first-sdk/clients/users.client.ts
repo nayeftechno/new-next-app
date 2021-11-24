8e1461b3975f9adc1fa8705d3afceffdfb20076e
Mohammad Nayef <nayeftechno@gmail.com>
2021-11-24 11:17:47 +0200

    Commit Changes

--- packages/first-sdk/clients/users.client.ts
+++ packages/first-sdk/clients/users.client.ts
@@ -3,6 +3,10 @@ const UsersClient={
     async getAllUsers(){
         const response = await $http.get("/users");
         return response;
+    },
+    async getSingleUser(id : number){
+        const response = await $http.get(`/users/${id}`);
+        return response;
     }
 };
 export {UsersClient};
